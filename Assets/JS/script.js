aefefa62464c7c3ab5e47460baaa5149d11ca735
Daniel Mrva <mrva.paints@gmail.com>
2022-01-29 21:26:08 -0700

    got color-coding working, added event listener, got local storage working, got local storage rendered on refresh

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,41 +1,13 @@
-// User Story
-// AS AN employee with a busy schedule
-// I WANT to add important events to a daily planner
-// SO THAT I can manage my time effectively
-
-// Acceptance Criteria
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
-//moment.js to display day/date at top of calendar
-//create timeblock cards for 8am - 6pm time slots.
-    // if current time (h) > time block time = past color
-    // if current time (h) === time block time = current color
-    // if current time (h) < time block time = future color
-    // if current time not in time blocks, no color?...
-//text input boxes for each time box
-    // local storage function for each time block, with key for each hour?
-    //save button for local storage can probably use event.target to get the buttons associated hour block, and therefore have only one event listener for the save buttons?...
-    //render function that checks local storage status and renders any saved inputs
-
-var hourNow = moment().hour(14).format("HH");
+//global variables
+var hourNow = moment().format("HH");
 var dayNow = moment().format("MMM, DD, YYYY");
 $("#currentDay").text(dayNow);
 var workHours = ["08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18"];
 
 console.log(hourNow);
 
+//general functions
+    //sets the colors for the hour slots
 function pastPresentFuture() {
     for (let index = 0; index < workHours.length; index++) {
         var workHour = document.getElementById(`t${workHours[index]}`).dataset.hour;
@@ -56,3 +28,27 @@ function pastPresentFuture() {
 
 pastPresentFuture();
 
+    //renders any activities saved in local storage
+function renderActivities() {
+    for (let j = 0; j < workHours.length; j++) {
+        var hourActivity = workHours[j];
+        var hourSaved = document.getElementById(`t${hourActivity}`);
+        var storedActivity = JSON.parse(localStorage.getItem(`${hourActivity}th`));
+        if (storedActivity !== null) {
+        hourSaved.setAttribute("value", storedActivity);
+        }
+    }
+};
+
+renderActivities();
+
+//event listeners
+
+document.addEventListener("click", function (event) {
+    if (event.target.matches(".btn")) {
+        var buttonHour = event.target.dataset.btn;
+        console.log(buttonHour);
+        var hourInput = document.getElementById(`t${buttonHour}`).value;
+        localStorage.setItem(`${buttonHour}th`, JSON.stringify(hourInput))
+    }
+});
